adc0d0e92cc022a903d8cf915b27eac474bedb6f
Dinesh sheelam <dineshsheelam459@gmail.com>
2022-07-25 14:09:23 +0530

    Changed commit

---
+++ dir1/add.py
@@ -0,0 +1,14 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================================================================================== RESTART: Shell ======================================================================================
+>>> # This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print(sum)
